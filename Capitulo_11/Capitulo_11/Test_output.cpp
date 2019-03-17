ab9562c6a4455898fab32a764ae8a0092eea1568
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2019-03-16 19:34:10 -0600

    .

--- Capitulo_11/Capitulo_11/Test_output.cpp
+++ Capitulo_11/Capitulo_11/Test_output.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <iomanip> 
+#include <ctype.h> //para toupper y tolower
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -10,17 +13,30 @@ void octal();
 void ocho();
 void diez();
 
+void ejercicios();
+void ejer1();
+void ejer3();
+void ejer4();
+void ejer6();
+void ejer7();
+void Hexa();
+void Octa();
+void Deci();
+
 int main()
 {
 	int valor = 0;
 
-	cout << " 1 Drill\n " << endl;
+	cout << " 1 Drill\n 2 Ejercicios" << endl;
 	cin >> valor;
 
 	switch (valor)
 	{
 		case 1:
 			drill();
+			break;
+		case 2:
+			ejercicios();
 			break;
 	default:
 		cout << "Valor fuera de rango" << endl;
@@ -30,7 +46,6 @@ int main()
 	cin.get();
 	return 0;
 }
-
 //Drill
 void drill()
 {
@@ -61,7 +76,6 @@ void drill()
 	cin.ignore();
 	cin.get();
 }
-
 void hexadecimal()
 {
 	int Año = 0;
@@ -128,7 +142,6 @@ void hexadecimal()
 	cin.ignore();
 	cin.get();
 }
-
 void octal()
 {
 	int Valor = 0;
@@ -150,7 +163,6 @@ void octal()
 	cin.ignore();
 	cin.get();
 }
-
 void ocho()
 {
 	int a, b, c, d = 0;
@@ -161,12 +173,307 @@ void ocho()
 	cin.ignore();
 	cin.get();
 }
-
 void diez()
 {
-
 	cout << "Nombre:" << '|' << setw(7) << "Ramses" << '|' << "Apellido paterno:" << '|' << setw(9) << "Guerrero" << endl;
-
 	cin.ignore();
 	cin.get();
+}
+//----------------------------------------------------------------------		  -----------------------------------------------------------------------
+//----------------------------------------------------------------------		  -----------------------------------------------------------------------
+//----------------------------------------------------------------------		  -----------------------------------------------------------------------
+//----------------------------------------------------------------------Ejercicios-----------------------------------------------------------------------
+void ejercicios()
+{
+	int valor = 0;
+
+	cout << " Ingresa\n 1-Ejer 1\n 2-Ejer 2\n 3-Ejer 4\n 4-Ejer6\n 5-Ejer 7\n " << endl;
+	cin >> valor;
+
+	switch (valor)
+	{
+		case 1:
+			ejer1();
+			break;
+		case 2:
+			ejer3();
+			break;
+		case 3:
+			ejer4();
+			break;
+		case 4:
+			ejer6();
+			break;
+		case 5:
+			ejer7();
+			break;
+	default:
+		cout << "Error, valor fuera de rango" << endl;
+		break;
+	}
+	cin.ignore();
+	cin.get();
+}
+//--------------------------------------------------------------------------------
+void ejer1()
+{
+	string palabra;
+	string Palab;
+	ofstream crear;
+
+	cout << "Ingresa una palabra en minuscula" << endl;
+	cin >> palabra;
+	Palab = palabra;
+	for (int i = 0; i < palabra.size(); i++)
+	{
+		Palab[i] = toupper(palabra[i]);
+	}
+
+	crear.open("ejer1.txt", ios::out);
+	crear << Palab;
+	crear.close();
+	cin.ignore();
+	cin.get();
+}
+//----------------------------------------------------------------------------------------
+void ejer3()
+{
+	string palabra;
+	cout << "Ingresa la palabra para eliminar vocales" << endl;
+	cin >> palabra;
+	//Ciclo para que en cuanto entre una vocal darle un valor de 0
+	for (int i = 0; i < palabra.size(); i++)
+	{
+		if (palabra[i] == 'a')
+		{
+			palabra[i] = 0;
+		}
+		if (palabra[i] == 'e')
+		{
+			palabra[i] = 0;
+		}
+		if (palabra[i] == 'i')
+		{
+			palabra[i] = 0;
+		}
+		if (palabra[i] == 'o')
+		{
+			palabra[i] = 0;
+		}
+		if (palabra[i] == 'u')
+		{
+			palabra[i] = 0;
+		}
+		
+	}
+	//Ciclo de impresion todo diferente a 0
+	for (int i = 0; i < palabra.size(); i++)
+	{
+		if (palabra[i] != 0)
+		{
+			cout << palabra[i];
+		}
+	}
+	cin.ignore();
+	cin.get();
+}
+//-------------------------------------------------------------------------------------------------------
+void ejer4()
+{
+	int valor = 0;
+
+	cout << " Ingresa 1 para ingresar un numero hexadeciamal\n 2 para un octal\n 3 para un deciaml" << endl;
+	cin >> valor;
+
+	switch (valor)
+	{
+		case 1:
+			Hexa();
+			break;
+		case 2:
+			Octa();
+			break;
+		case 3:
+			Deci();
+			break;
+
+	default:
+		cout << "Valor fuera de rango" << endl;
+		break;
+	}
+	cin.ignore();
+	cin.get();
+}
+void Hexa()
+{
+	int ValorHex;
+	
+	cout << "Ingresa un valor hexadecimal" << endl;
+	cin >> hex >> ValorHex;
+	cout << ValorHex << endl;
+	cin.ignore();
+	cin.get();
+}
+void Octa()
+{
+	int ValorOctal = 0;
+	int ValorDecimal = 0;
+	int Rem = 0;
+	int i = 0;
+
+	cout << "Ingresa el valor octal" << endl;
+	cin >> ValorOctal;
+
+	while (ValorOctal != 0)
+	{
+		Rem = ValorOctal % 10;
+		ValorOctal /= 10;
+		ValorDecimal += Rem * pow(8,i);
+		i++;
+	}
+	cout << ValorDecimal << endl;
+	cin.ignore();
+	cin.get();
+}
+void Deci()
+{
+	int ValorDeci = 0;
+
+	cout << "Ingresa el valor decimal" << endl;
+	cin >> ValorDeci;
+
+	cout << ValorDeci << endl;
+	cin.ignore();
+	cin.get();
+}
+//-------------------------------------------------------------------------------------------
+void ejer6()
+{
+	string Frase;
+	const char NoDeseado[] = {'.', ';', ',', '?', '-'};
+	int Switch = 0;
+
+	cout << "Ingresa la frase" << endl;
+	cin >> Frase;
+
+	for (int i = 0; i < Frase.size(); i++)
+	{
+		for (int j = 0; j < strlen(NoDeseado); j++)
+		{
+			if (Frase[i] == NoDeseado[j])
+			{
+				Switch = 1;
+			}
+		}
+		if (Switch == 0)
+		{
+			cout << Frase[i];
+		}
+		Switch = 0;
+	}
+	cout << '\n';
+	cin.ignore();
+	cin.get();
+}
+//-----------------------------------------------------------------------
+void ejer7()
+{
+	string Frase;
+	const char NoDeseado[] = { '.', ';', ',', '?', '-' };
+	int Switch = 0;
+
+	cout << "Ingresa la frase" << endl;
+	cin >> Frase;
+
+	for (int i = 0; i < Frase.size(); i++)
+	{
+		for (int j = 0; j < strlen(NoDeseado); j++)
+		{
+			if (Frase[i] == NoDeseado[j])
+			{
+				Switch = 1;
+			}
+		}
+		if (Switch == 0)
+		{
+			cout << Frase[i];
+		}
+		Switch = 0;
+	}
+	cout << '\n';
+	cin.ignore();
+	cin.get();
+}
+//-----------------------------------------------------------------------
+void ejer8()
+{
+	ofstream Crear;
+	string Frase;
+	const char NoDeseado[] = { '.', ';', ',', '?', '-' };
+	int Switch = 0;
+
+	Crear.open("ejer8.txt", ios::out);
+	cout << "Ingresa la frase" << endl;
+	cin >> Frase;
+
+	for (int i = 0; i < Frase.size(); i++)
+	{
+		for (int j = 0; j < strlen(NoDeseado); j++)
+		{
+			if (Frase[i] == NoDeseado[j])
+			{
+				Switch = 1;
+			}
+		}
+		if (Switch == 0)
+		{
+			cout << Frase[i];
+			Crear << Frase[i];
+		}
+		Switch = 0;
+	}
+	cout << '\n';
+
+	Crear.close();
+	cin.ignore();
+	cin.get();
+}
+//---------------------------------------------------------
+void ejer9()
+{
+	string iname;
+	string oname;
+	string as_bytes;
+	vector<int> v;
+
+	// open an istream for binary input from a file:
+	cout << "Please enter input file name\n";
+	cin >> iname;
+	// note: stream mode
+	ifstream ifs{ iname,ios_base::binary };
+	// binary tells the stream not to try anything clever with the bytes
+	if (!ifs)
+	{
+		cout << "can't open input file "<< iname << endl;
+	}
+	// open an ostream for binary output to a file:
+	cout << "Please enter output file name\n";
+	cin >> oname;
+	ofstream ofs{ oname,ios_base::binary }; // note: stream mode
+	// binary tells the stream not to try anything clever with the bytes
+	if (!ofs)
+	{
+		cout << "can't open output file " << oname << endl;
+	}
+	// read from binary file:
+	//for (int x; ifs.read(as_bytes(x), sizeof(int)); ) // note: reading bytes
+	{
+		//v.push_back(x);
+	}
+	// . . . do something with v . . .
+	// write to binary file:
+	for (int x : v)
+	{
+		//ofs.write(as_bytes(x), sizeof(int));
+	}
 }
